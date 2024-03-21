3565f4885ccbf583795a0b92a49c9e9b9da47ebb
Cerpelloni Massimo <massimo.cerpelloni@idbverona>
2024-03-21 11:54:55 +0100

    modifica

--- reader.c
+++ reader.c
@@ -9,6 +9,13 @@
 #define BLOCK_DIM 1024
 #define PROGRAM "writer"
 
+/*!
+* \fn spawn 
+* \breif crea il processo
+* \param(in) program (char)
+* \param(in) argument (char)
+* \param(out) int
+*/
 int spawn(char program[], char* argument[])
 {
  int pid;
@@ -22,6 +29,7 @@ int spawn(char program[], char* argument[])
  abort();
 }
 
+//main quindi l'inizio
 int main(int argc, char* argv[])
 {
  FILE* file;
@@ -36,7 +44,7 @@ int main(int argc, char* argv[])
   return 0;
  }
  arg[0] = (char*)malloc(strlen(PROGRAM) + 1);
- strcpy(arg[0], PROGRAM);
+ strcpy(arg[0], PROGRAM);//copia la stringa nel vettore?
  arg[1] = (char*)malloc(strlen(argv[2]) + 1);
  strcpy(arg[1], argv[2]);
  arg[2] = NULL;
@@ -45,7 +53,7 @@ int main(int argc, char* argv[])
  if (spawn(PROGRAM, arg) < 0)
  {
   printf("Errore creazione processo\r\n");
-  free(arg[0]);
+  free(arg[0]);//immagino libera lo spazio che gli viene dato
   free(arg[1]);
   return 0;
  }
